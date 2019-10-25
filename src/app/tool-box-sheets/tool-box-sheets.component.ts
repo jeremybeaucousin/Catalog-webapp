705df7a1f3ce3fc94864962654bc343d65760a1c
Jérémy BEAUCOUSIN <JBEAUCOU@groupement.systeme-u.fr>
2019-10-25 12:16:59 +0200

    Add form builder to handle data refreshing

--- src/app/tool-box-sheets/tool-box-sheets.component.ts
+++ src/app/tool-box-sheets/tool-box-sheets.component.ts
@@ -1,10 +1,10 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewChildren, QueryList, Directive, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
-import { MatPaginator, MatSort, MatInput, MatTableDataSource, MatDialog } from '@angular/material';
+import { MatPaginator, MatSort, MatInput, MatTableDataSource, MatDialog, MatButton } from '@angular/material';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
-import { merge, of as observableOf } from 'rxjs';
+import { merge, of as observableOf, Observable, Subscription } from 'rxjs';
 import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 
 import { CatalogApiService } from '../services/catalog-api.service';
@@ -15,6 +15,7 @@ import { SnackBarAppService } from '../services/snack-bar-app.services';
 import { MatPaginatorTranslator } from '../models/mat-paginator-translator';
 import { TranslationKeysConstants } from '../models/translation-keys.constants';
 import { AuthenticationService } from '../services/authentication-service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tool-box-sheets',
@@ -28,7 +29,7 @@ import { AuthenticationService } from '../services/authentication-service';
     ]),
   ],
 })
-export class ToolBoxSheetsComponent implements AfterViewInit {
+export class ToolBoxSheetsComponent implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['title', 'description', "actions"];
   dataSource: MatTableDataSource<ToolBoxSheet>;
 
@@ -36,32 +37,47 @@ export class ToolBoxSheetsComponent implements AfterViewInit {
   isLoadingResults = true;
   isRateLimitReached = false;
   expandedRow;
+  observableInputs: Observable<object | any[]>;
+
+  searchForm: FormGroup;
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
-  @ViewChild(MatInput, { static: false }) matInput: MatInput;
 
   constructor(
     private catalogService: CatalogApiService,
     private _snackBar: SnackBarAppService,
     public dialog: MatDialog,
     private translate: TranslateService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
   ) {
+  }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      search: [''],
+    });
+
+    this.dataSource = new MatTableDataSource([]);
   }
 
   ngAfterViewInit() {
     this.paginator._intl = new MatPaginatorTranslator(this.translate);
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    merge(this.sort.sortChange, this.paginator.page, this.matInput.stateChanges)
+    // If the user changes the sort order or the filter, reset back to the first page.
+    merge(this.sort.sortChange, this.searchForm.valueChanges).subscribe(
+      () => {
+        this.paginator.pageIndex = 0;
+      });
+    this.observableInputs = merge(this.sort.sortChange, this.paginator.page, this.searchForm.valueChanges)
       .pipe(
         startWith({}),
         switchMap(() => {
           this.isLoadingResults = true;
           const offset = (this.paginator.pageIndex * this.paginator.pageSize);
           const limit = this.paginator.pageSize;
-          const wordSequence = this.matInput.value;
+          const wordSequence = (this.searchForm.value) ? this.searchForm.value.search : "";
+          console.log("refresh");
           return this.catalogService.getToolBoxSheets(offset, limit, this.sort.active, this.sort.direction, wordSequence);
         }),
         map((response) => {
@@ -79,10 +95,11 @@ export class ToolBoxSheetsComponent implements AfterViewInit {
           this.isRateLimitReached = true;
           return observableOf([]);
         })
-      ).subscribe(data => {
-        var datasource = new MatTableDataSource(data as Array<ToolBoxSheet>);
-        this.dataSource = datasource;
-      });
+      );
+
+    this.observableInputs.subscribe(data => {
+      this.dataSource.data = data as Array<ToolBoxSheet>;
+    })
   }
 
   hasAdminPermission() {
