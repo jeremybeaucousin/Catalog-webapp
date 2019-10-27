79d8bec6d9ce46c10c34e50fa46c1ab3665814cb
Jérémy BEAUCOUSIN <jeremy.beaucousin@gmail.com>
2019-10-27 08:19:05 +0100

    Add app version on main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthenticationService } from './services/authentication-service';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,18 +11,17 @@ import { AuthenticationService } from './services/authentication-service';
 })
 export class AppComponent {
   title = 'Catalog-webapp';
+  appVersion = environment.appVersion;
 
   constructor(
     @Inject(LOCALE_ID) public locale: string,
     private translate: TranslateService,
     private authenticationService: AuthenticationService) {
+    // Choose langage depending on packaged local for translate service 
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang("fr");
     if (translate.getLangs().includes(locale)) {
       translate.use(locale);
-    }
-    window.onbeforeunload = function () {
-      authenticationService.logout();
     }
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import { version } from '../../package.json';
+
 export const environment = {
   production: true,
+  appVersion: version,
   catalogApiEndploint: "https://109.18.10.254:9001/"
 };
